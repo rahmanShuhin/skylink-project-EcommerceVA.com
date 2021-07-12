891f282069a3eea2b8474a5f63fa511d2e0ffec4
mozumderTushar <itztushar55@gmail.com>
2021-07-12 17:29:48 +0600

    need to change color

--- src/App.js
+++ src/App.js
@@ -14,62 +14,66 @@ import GetStart from "./components/GetStart/GetStart";
 import Footer from "./components/Footer/Footer";
 import Contact from "./components/Contact/Contact";
 import { useEffect } from "react";
+import Home from "./components/Home/Home";
 
 function App() {
-    useEffect(() => {
-        AOS.init({ duration: 2000 });
-    }, []);
-    return (
-        <Router>
-            <Switch>
-                <div className="App">
-                    <Nav></Nav>
-                    <Route exact path="/">
-                        {/* for  home routing  */}
-                    </Route>
-                    <Route exact path="/amazon/">
-                        <Landing
-                            title={"Get Fully Automated"}
-                            after={"Amazon Store"}
-                        ></Landing>
-                        <Services></Services>
-                        <WatchVideo></WatchVideo>
-                        <Automated></Automated>
-                        <Results></Results>
-                        <GetStart></GetStart>
-                    </Route>
-                    <Route exact path="/walmart/">
-                        <Landing
-                            title={"Get Your Own"}
-                            after={"Automated Walmart Store"}
-                        ></Landing>
-                        <Services></Services>
-                        <WatchVideo></WatchVideo>
-                        <Automated></Automated>
-                        <Results></Results>
-                        <GetStart></GetStart>
-                        <Contact></Contact>
-                    </Route>
-                    <Route exact path="/fb-dropship/">
-                        <Landing
-                            title={"Get Your Own"}
-                            after={"FB Dropship Store"}
-                        ></Landing>
-                        <Services></Services>
-                        <WatchVideo></WatchVideo>
-                        <Automated></Automated>
-                        <Results></Results>
-                        <GetStart></GetStart>
-                        <Contact></Contact>
-                    </Route>
-                    <Route exact path="/request-a-call/">
-                        <Contact></Contact>
-                    </Route>
-                    <Footer></Footer>
-                </div>
-            </Switch>
-        </Router>
-    );
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+  return (
+    <Router>
+      <Switch>
+        <div className="App">
+          <Nav></Nav>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/amazon/">
+            <Landing
+              title={"Get Fully Automated"}
+              after={"Amazon Store"}
+              description={"Through our Management Service, you will be able to create,modify, and scale 100% of your E-Commerce business."}
+            ></Landing>
+            <Services></Services>
+            <WatchVideo></WatchVideo>
+            <Automated></Automated>
+            <Results></Results>
+            <GetStart></GetStart>
+          </Route>
+          <Route exact path="/walmart/">
+            <Landing
+              title={"Get Your Own"}
+              after={"Automated Walmart Store"}
+              description={"Through our Management Service, you will be able to create,modify, and scale 100% of your E-Commerce business."}
+            ></Landing>
+            <Services></Services>
+            <WatchVideo></WatchVideo>
+            <Automated></Automated>
+            <Results></Results>
+            <GetStart></GetStart>
+            <Contact></Contact>
+          </Route>
+          <Route exact path="/fb-dropship/">
+            <Landing
+              title={"Get Your Own"}
+              after={"FB Dropship Store"}
+              description={"Through our Management Service, you will be able to create,modify, and scale 100% of your E-Commerce business."}
+            ></Landing>
+            <Services></Services>
+            <WatchVideo></WatchVideo>
+            <Automated></Automated>
+            <Results></Results>
+            <GetStart></GetStart>
+            <Contact></Contact>
+          </Route>
+          <Route exact path="/request-a-call/">
+            <Contact></Contact>
+          </Route>
+          <Footer></Footer>
+        </div>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
