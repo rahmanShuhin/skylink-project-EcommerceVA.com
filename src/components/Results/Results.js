b00d7c9b4aaf1dda3e3fae1c0df01e4d174b78dd
rahmanShuhin <suhin4000@gmail.com>
2021-07-16 20:16:07 +0600

    slider added

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import arrow from "../../../src/images/Arrows---Copy_orrange.png";
 import results from "../../../src/images/1.png";
@@ -8,8 +8,32 @@ import results4 from "../../../src/images/4.png";
 import results5 from "../../../src/images/5.png";
 import results6 from "../../../src/images/6.png";
 import Horizontal from "../Horizontal/Horizontal";
+import SwiperCore, { Pagination, Navigation, Autoplay } from "swiper/core";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/swiper.min.css";
+
+import "swiper/components/navigation/navigation.min.css";
+import { useState } from "react";
+
+// install Swiper components
+SwiperCore.use([Pagination, Navigation, Autoplay]);
 const Results = () => {
+    const [number, setNumber] = useState(2);
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            window.addEventListener("scroll", (event) => {
+                var w = window.innerWidth;
+                if (w < 720) {
+                    setNumber(1);
+                } else {
+                    setNumber(2);
+                }
+            });
+        }
+    });
     return (
         <div className="results">
             <div className="__results__top" data-aos="fade-up">
@@ -18,69 +42,33 @@ const Results = () => {
             <div className="__results__img" data-aos="fade-up">
                 <img src={arrow} alt="" />
             </div>
+
             <div className="__results__bottom__img">
-                <Container>
-                    <div className="main__img" data-aos="fade-up">
-                        <Row>
-                            <Col md={6}>
-                                <img
-                                    src={results}
-                                    alt=""
-                                    className="img-fluid"
-                                    data-aos="fade-right"
-                                />
-                            </Col>
-                            <Col md={6}>
-                                <img
-                                    src={results2}
-                                    alt=""
-                                    className="img-fluid"
-                                    data-aos="fade-left"
-                                />
-                            </Col>
-                        </Row>
-                    </div>
-                    <div className="__section__one__img">
-                        <Row>
-                            <Col md={6}>
-                                <img
-                                    src={results3}
-                                    alt=""
-                                    className="img-fluid"
-                                    data-aos="fade-right"
-                                />
-                            </Col>
-                            <Col md={6}>
-                                <img
-                                    src={results4}
-                                    alt=""
-                                    className="img-fluid"
-                                    data-aos="fade-left"
-                                />
-                            </Col>
-                        </Row>
-                    </div>
-                    <div className="__section__two__img">
-                        <Row>
-                            <Col md={6}>
-                                <img
-                                    src={results5}
-                                    alt=""
-                                    className="img-fluid"
-                                    data-aos="fade-right"
-                                />
-                            </Col>
-                            <Col md={6}>
-                                <img
-                                    src={results6}
-                                    alt=""
-                                    className="img-fluid"
-                                    data-aos="fade-left"
-                                />
-                            </Col>
-                        </Row>
-                    </div>
-                </Container>
+                <Swiper
+                    navigation
+                    Autoplay={true}
+                    spaceBetween={50}
+                    slidesPerView={number}
+                >
+                    <SwiperSlide>
+                        <img src={results} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={results2} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={results3} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={results4} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={results5} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={results6} alt="" />
+                    </SwiperSlide>
+                </Swiper>
             </div>
             <div data-aos="fade-up">
                 <Horizontal></Horizontal>
